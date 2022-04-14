aabf90a3d7a5d5cfdaad17a4bd32b5d4434571be
Brent Steffen <BESteffen82@gmail.com>
2022-04-14 11:31:07 -0400

    Edit rps.js

--- rps.js
+++ rps.js
@@ -13,19 +13,36 @@ function computerPlay () {
 function playRound (playerSelection, computerSelection ) {
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissor'){
       return 'You Win! Rock beats Scissors'};
-    if (playerSelection.toLowerCase() === 'rock' &&computerSelection === 'paper'){
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
       return 'You Lose! Paper beats Rock'};
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock'){
-        return 'You Tie!';
-    }
+        return 'You Tie!'};
+    if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissor'){
+        return 'You Lose! Scissor beats Paper'};
+    if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock'){
+        return 'You Win! Paper beats Rock'};
+    if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper'){
+        return 'You Tie!'};
+    if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper'){
+        return 'You Win! Scissors beat Paper'};
+    if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock'){
+        return 'You Lose! Rock beats Scissors'}
+    if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors'){
+        return 'You Tie'}
+        else {return 'Please input either Rock, Paper or Scissors!'};                  
 }    
-    let computerSelection = computerPlay();
-    let playerSelection = prompt('Rock, Paper, Scissors');
+
+function game() {
+    
+}
+    
+let computerSelection = computerPlay();
+let playerSelection = prompt('Rock, Paper, Scissors');
         
 
 
 
-console.log(playRound(playerSelection,computerSelection));
+
 
 
 
