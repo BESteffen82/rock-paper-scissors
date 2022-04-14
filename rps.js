87ecf11e82ccf6a55ada3324a538fb4f071b4d64
Brent Steffen <BESteffen82@gmail.com>
2022-04-14 10:39:35 -0400

    Edit rps.html and rps.js

--- rps.js
+++ rps.js
@@ -1,23 +1,31 @@
 function computerPlay () {
     let result = Math.floor(Math.random()*3)
     if (result === 0) {
-        return 'Rock'
+        return 'rock';
     }
     if (result === 1) {
-        return 'Paper'
+        return 'paper';
     }
-    else {return 'Scissor'}
+    if (result === 2){
+        return 'scissor'};
 }
 
 function playRound (playerSelection, computerSelection ) {
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissor'){
+      return 'You Win! Rock beats Scissors'};
+    if (playerSelection.toLowerCase() === 'rock' &&computerSelection === 'paper'){
+      return 'You Lose! Paper beats Rock'};
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock'){
+        return 'You Tie!';
+    }
+}    
+    let computerSelection = computerPlay();
+    let playerSelection = prompt('Rock, Paper, Scissors');
+        
 
-}
-
-const computerSelection = computerPlay();
-let playerSelection = prompt('Rock, Paper, or Scissors');
 
 
-
+console.log(playRound(playerSelection,computerSelection));
 
 
 
