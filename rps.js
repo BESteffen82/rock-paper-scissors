67f14ce692635e6b76b6a4b61693f8f1b08835ec
Brent Steffen <BESteffen82@gmail.com>
2022-04-15 10:33:07 -0400

    Edit rps.js

--- rps.js
+++ rps.js
@@ -1,3 +1,10 @@
+let user;
+let computer;
+let win = 1;
+let loss = 1;
+let tie = 1;
+
+
 function computerPlay () {
     let result = Math.floor(Math.random()*3)
     if (result === 0) {
@@ -7,14 +14,17 @@ function computerPlay () {
         return 'paper';
     }
     if (result === 2){
-        return 'scissor'};
-}
+        return 'scissor'};        
+} 
 
-function playRound (playerSelection, computerSelection ) {
+let computerSelection = computerPlay();
+
+function playRound (playerSelection, computerSelection) { 
+
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissor'){
-      return 'You Win! Rock beats Scissors'};
+    return 'You Win! Rock beats Scissors'};
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
-      return 'You Lose! Paper beats Rock'};
+    return 'You Lose! Paper beats Rock'};
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock'){
         return 'You Tie!'};
     if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissor'){
@@ -29,15 +39,47 @@ function playRound (playerSelection, computerSelection ) {
         return 'You Lose! Rock beats Scissors'}
     if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors'){
         return 'You Tie'}
-        else {return 'Please input either Rock, Paper or Scissors!'};                  
+        else {return 'Please input either Rock, Paper or Scissors!'};
 }    
 
-function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Rock', 'Paper', 'Scissors');
+        let computerSelection = computerPlay();
+        
+        if (i === 0){
+        console.log(playRound(playerSelection, computerSelection));
+        }
+        if (i === 1) {
+            console.log(playRound(playerSelection, computerSelection));
+        }
+        if (i === 2) {
+            console.log(playRound(playerSelection, computerSelection));
+        }
+        if (i === 3) {
+            console.log(playRound(playerSelection, computerSelection));
+        }
+        if (i === 4) {      
+            console.log(playRound(playerSelection, computerSelection));
+        }    
+    }
+
+
+           
     
-}
     
-let computerSelection = computerPlay();
-let playerSelection = prompt('Rock, Paper, Scissors');
+    
+
+
+
+
+
+
+
+
+
+
+
+
         
 
 
