2d9b947d479c9e61a7a26c8ba272ed6e4570e423
Brent Steffen <BESteffen82@gmail.com>
2022-04-21 11:13:58 -0400

    Add buttons to GUI

--- rps.js
+++ rps.js
@@ -12,18 +12,29 @@ function computerPlay() {
         return 'paper';
     }
     if (result === 2){
-        return 'scissor'};        
+        return 'scissors'};        
 } 
 
+const roundResult = document.getElementById('round-result');
 
-
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+    updatedPlayer = button.id;
+    computerSelection = computerPlay();
+    console.log(playRound(updatedPlayer, computerSelection));
+    console.log(`Player:${playerScore} Computer:${computerScore}`);
+    }); 
+});
+  
 
 function playRound(updatedPlayer, computerSelection) {
     let a = 'You Win!' + ' ' + updatedPlayer + ' ' + 'beats' + ' ' + computerSelection;
     let b = 'You Lose!' + ' ' + computerSelection + ' ' + 'beats' + ' ' + updatedPlayer;
     let c = 'You Tie!';
-    
-    if (updatedPlayer === 'rock' && computerSelection === 'scissor'){
+
+       
+    if (updatedPlayer === 'rock' && computerSelection === 'scissors'){
         playerScore+=1;
         return a;
         };
@@ -35,7 +46,7 @@ function playRound(updatedPlayer, computerSelection) {
         playerScore+=1;
         return a;
         };
-    if (updatedPlayer === 'paper' && computerSelection === 'scissor'){
+    if (updatedPlayer === 'paper' && computerSelection === 'scissors'){
         computerScore+=1;
         return b;
         };    
@@ -49,19 +60,19 @@ function playRound(updatedPlayer, computerSelection) {
         };
     if (updatedPlayer === computerSelection) {
         return c};
-}             
- 
-for (let i = 0; i < 5; i++){      
-    let playerSelection = prompt('Rock, Paper or Scissors?'); 
-    let updatedPlayer = playerSelection.toLowerCase();   
-    let computerSelection = computerPlay();   
-    
-    console.log(playRound(updatedPlayer, computerSelection));
-    console.log(`Player:${playerScore} Computer:${computerScore}`);    
         
+    }
+
+
+//for (let i = 0; i < 5; i++){      
+    //let playerSelection = prompt('Rock, Paper or Scissors?'); 
+    //let updatedPlayer = playerSelection.toLowerCase();   
+    //let computerSelection = computerPlay();   
     
-}      
-                       
+   //console.log(playRound(updatedPlayer, computerSelection));
+   // console.log(`Player:${playerScore} Computer:${computerScore}`);    
+//}      
+                     
 function gameWinner(playerScore, computerScore){
         if (`${playerScore}` > `${computerScore}`){
             return 'You Win!';
